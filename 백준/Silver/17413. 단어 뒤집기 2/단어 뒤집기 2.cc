3e8a0a68cc47a6bb5e8352faf2399a8dfbf91c95
dldb-chamchi <132354633+dldb-chamchi@users.noreply.github.com>
2025-01-15 22:22:44 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 8 ms, Memory: 2368 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -0,0 +1,48 @@
+//백준 17413, 단어 뒤집기 2
+#include <iostream>
+#include <stack>
+
+char string[100'000];
+std::stack<char> stack;
+
+int main(){
+
+    std::string s;
+    std::getline(std::cin, s);
+    bool flag = false;
+    for(int i{0}; i<s.size(); ++i){
+        if(s[i] == ' '){
+            while(!stack.empty()){
+                std::cout << stack.top();
+                stack.pop();
+            }
+            std::cout << ' ';
+        }
+        else if(s[i] == '<'){
+            while(!stack.empty()){
+                std::cout << stack.top();
+                stack.pop();
+            }
+            flag = true;
+            std::cout << s[i];
+        }
+        else if(s[i] == '>'){
+            flag = false;
+            std::cout << s[i];
+        }
+        else if(flag) std::cout << s[i];
+        else stack.push(s[i]);
+    }
+
+    while(!stack.empty()){
+            std::cout << stack.top();
+            stack.pop();
+        }
+        std::cout << ' ';
+
+
+
+
+
+    return 0;
+}
