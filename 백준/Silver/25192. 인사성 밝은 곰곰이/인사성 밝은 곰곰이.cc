d406c27c7bede01e87c40effc354b7bfda2e07c4
임아리 <dladkfl49@gmail.com>
2025-01-23 17:24:36 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 88 ms, Memory: 8760 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -1,33 +1,29 @@
-//백준 25192, 인사성 밝은 곰곰이
-
 #include <iostream>
-#include <unordered_map>
+#include <set>
+using namespace std;
 
-std::unordered_map<std::string, int> map;
+int main()
+{
+	int N, count = 0;
+	cin >> N;
+	set<string> m;
+	string str;
 
-int main(){
-    std::ios_base :: sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+	while (N--)
+	{
+		cin >> str;
 
-    int N;
-    std::cin >> N;
-    std::string s;
+		if (str == "ENTER")
+		{
+			count += m.size();
+			m.clear();
+			continue;
+		}
+		
+		m.insert(str);
+	}
 
-    int gom{0};
-    for(int i{0}; i<N; ++i){
-        std::cin >> s;
-        if(s == "ENTER"){
-            map.clear();
-        }
-        else{
-            if(map.find(s) != map.end()) continue;
-            map[s] = 1;
-            ++gom;
-        }
-    }
+	count += m.size();
 
-    std::cout << gom;
-
-    return 0;
+	cout << count;
 }
