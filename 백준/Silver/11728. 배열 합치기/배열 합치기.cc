d6715b1c5f032ec6a1ffad175120b10b652e8ef3
임아리 <dladkfl49@gmail.com>
2025-01-25 10:36:55 +0900

    [Silver V] Title: 배열 합치기, Time: 1296 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,58 +1,26 @@
 //백준 11728, 배열 합치기
 #include <iostream>
 
-int arr[2'000'001];
-int arr1[1'000'000];
-int arr2[1'000'000];
+int A[1'000'001];
+int B[1'000'001];
 
 int main(){
 
     int N, M;
 
     std::cin >> N >> M;
+    for(int i{0}; i<N; ++i) std::cin >> A[i];
     
-    int idx1{0}; int idx2{0};
-    int n;
-
-    for(int i{0}; i<N; ++i){
-        std::cin >> arr1[i];
-    }
-
-    for(int i{0}; i<M; ++i){
-        std::cin >> arr2[i];
-    }
-
-    for(int i{0}; i<N+M; ++i){
-        if(idx1 == N || idx2 == M){
-            n = i;
-            break;
-        }
-        if(arr1[idx1] < arr2[idx2]){
-            arr[i] = arr1[idx1];
-            ++idx1;
-        }
-        else{
-            arr[i] = arr2[idx2];
-            ++idx2;
-        }
-    }
-    if(idx1 == N){
-        for(int i{n}; i<N+M; ++i){
-            arr[i] = arr2[idx2];
-            ++idx2;
-        }
-    }
-    else{
-        for(int i{n}; i<N+M; ++i){
-            arr[i] = arr1[idx1];
-            ++idx1;
-        }
-    }
-
-    for(int i{0}; i<N+M; ++i){
-        std::cout << arr[i] << ' ';
-    }
+    for(int i{0}; i<M; ++i) std::cin >> B[i];
     
+    int Aidx{0}; int Bidx{0};
+    for(int i{0}; i<N+M; ++i){
+        if(Aidx == N) std::cout << B[Bidx++];
+        else if(Bidx == M) std::cout << A[Aidx++];
+        else if(A[Aidx] > B[Bidx]) std::cout << B[Bidx++];
+        else std::cout << A[Aidx++];
+        std::cout << ' ';
+    }
 
     return 0;
 }
