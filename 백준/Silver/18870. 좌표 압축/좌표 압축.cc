9ae12e6188cbd10ca42a5ca7acfbaa3f0ceb961f
임아리 <dladkfl49@gmail.com>
2025-01-26 23:56:41 +0900

    [Silver II] Title: 좌표 압축, Time: 800 ms, Memory: 45744 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,31 +1,47 @@
+//백준 18870, 좌표 압축
+
 #include <iostream>
+#include <set>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 
-int main() {
-    int N;
+int arr[1'000'000];
+int copy[1'000'000];
+std::vector<int> sorted;
+int N;
+std::set<int> set;
+
+void bs(int i){
+    int st{0}; int end = sorted.size();
+    while(st < end){
+        int mid = (st+end) / 2;
+        if(sorted[mid] >= arr[i]) end = mid;
+        else st = mid +1;  
+    }
+    std::cout << st << ' ';
+}
+
+int main (){
+    
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
     std::cin >> N;
-
-    std::vector<int> nums(N);
-    for (int i = 0; i < N; ++i) {
-        std::cin >> nums[i];
+    for(int i{0}; i<N; ++i) std::cin >> arr[i];
+    std::copy(arr, arr+N, copy);
+    for(int i{0}; i<N; ++i){
+        set.insert(copy[i]);
     }
 
-    // 정렬 및 중복 제거를 위한 복사본 생성
-    std::vector<int> sortedNums(nums);
-    std::sort(sortedNums.begin(), sortedNums.end());
-    sortedNums.erase(std::unique(sortedNums.begin(), sortedNums.end()), sortedNums.end());
-
-    // 좌표 압축을 위한 매핑 생성
-    std::unordered_map<int, int> coordMap;
-    for (int i = 0; i < sortedNums.size(); ++i) {
-        coordMap[sortedNums[i]] = i;
+    for(auto n : set){
+        sorted.push_back(n);
     }
 
-    // 좌표 압축 결과 출력
-    for (int i = 0; i < N; ++i) {
-        std::cout << coordMap[nums[i]] << " ";
+    std::sort(sorted.begin(), sorted.end());
+
+    for(int i{0}; i<N; ++i){
+        bs(i);
     }
 
     return 0;
