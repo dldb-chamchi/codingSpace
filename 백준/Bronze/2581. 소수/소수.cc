df307f8f0dc7d32b343614ae6b8bf44b55a82730
임아리 <dladkfl49@gmail.com>
2025-01-31 21:09:55 +0900

    [Bronze II] Title: 소수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.cc
+++ 백준/Bronze/2581. 소수/소수.cc
@@ -1,5 +1,6 @@
 //백준 2581, 소수
 #include <iostream>
+#include <cmath>
 
 int main(){
 
@@ -8,7 +9,7 @@ int main(){
     
     while(M <= N){
         int i{2}; bool flag = M == 1 ? true : false; 
-        while(i < M){
+        while(i <= std::sqrt(M)){ //제곱근까지만 봐도 됨(약수는 대칭적 성질이기 때문)
             if(M%i == 0){
                 flag = true;
                 break;
