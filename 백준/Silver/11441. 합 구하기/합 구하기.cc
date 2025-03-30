af7753021ad93167aa0bcce5c79abff7f868eedd
임아리 <dladkfl49@gmail.com>
2025-03-30 22:08:13 +0900

    [Silver III] Title: 합 구하기, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -0,0 +1,34 @@
+//백준 11441, 합 구하기
+#include <iostream>
+
+int arr[100'000];
+
+int main(){
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+
+    int N, M;
+    std::cin >> N;
+    for(int i{1}; i<=N; ++i){
+        std::cin >> arr[i];
+    }
+
+    std::cin >> M;
+
+    for(int i{1}; i<N; ++i){
+        arr[i+1] += arr[i];
+    }
+
+    int i, j;
+    for(int k{0}; k<M; ++k){
+        std::cin >> i >> j;
+        std::cout << arr[j] - arr[i-1] << '\n';
+    }
+
+
+
+    return 0;
+}
