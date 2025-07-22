272dba08b8d0c2e2ea8c67d3ffa09a85a2f0bfa2
임아리 <dladkfl49@gmail.com>
2025-07-22 11:29:16 +0900

    [Gold V] Title: 경로 게임, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/12887. 경로 게임/경로 게임.py
+++ 백준/Gold/12887. 경로 게임/경로 게임.py
@@ -3,14 +3,17 @@
 import sys
 input = sys.stdin.readline
 
+MAX = 9999
+
 def bfs(x, y):
     cnt = 1
-    nx = x; ny = y
-    while ny != M-1:
-        if grid[nx][ny+1] == '.':
-            ny += 1
+    if grid[x][y] != '.':
+        return MAX
+    while y != M-1:
+        if grid[x][y+1] == '.':
+            y += 1
         else:
-            nx = 0 if nx == 1 else 1
+            x = 0 if x == 1 else 1
         cnt += 1
     return cnt
 
@@ -19,10 +22,4 @@ grid = [list(input().rstrip()) for i in range(2)]
 
 white = sum(row.count('.') for row in grid)
 
-cnt1 = 9999; cnt2 = 9999
-if grid[0][0] == '.':
-    cnt1 = bfs(0, 0)
-if grid[1][0] == '.':
-    cnt2 = bfs(1, 0)
-
-print(white-min(cnt1, cnt2))
+print(white-min(bfs(0, 0), bfs(1, 0)))
