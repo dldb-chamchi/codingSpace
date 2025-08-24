4770abe12b93f1e5df2022f852d849e40d6b10ab
임아리 <dladkfl49@gmail.com>
2025-08-24 15:42:57 +0900

    [Gold V] Title: 퇴사 2, Time: 412 ms, Memory: 122744 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,5 +1,5 @@
 #백준 15486 퇴사 2
-
+'''
 import sys
 input = sys.stdin.readline
 
@@ -14,9 +14,29 @@ for i in range(N):
 maxP = 0
 for i in range(N):
     t, p = schedule[i][0], schedule[i][1]
-    maxP = max(maxP, dp[i])
+    maxP = max(maxP, dp[i]) #dp[i+1] = max(dp[i], dp[i+1])
     if i + t > N:
         continue
-    dp[i+t] = max(maxP+p, dp[i+t])
+    dp[i+t] = max(maxP+p, dp[i+t]) #dp[i+t] = max(dp[i+t], dp[i]+p)
+
+print(dp)
+print(max(dp))
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0 for _ in range(n+1)]
+
+for i in range(n):
+  t, p = map(int, input().split())
+
+  #dp[i]: i번째 날의 최대수익
+  dp[i] = max(dp[i], dp[i-1])
+  if (i+t < n+1):
+    dp[i+t] = max(dp[i+t], dp[i]+p)
+
 
 print(max(dp))
