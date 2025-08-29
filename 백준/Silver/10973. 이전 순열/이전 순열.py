c68688793523119dc99a6334770ab31ba7b1f66a
임아리 <dladkfl49@gmail.com>
2025-08-29 11:13:42 +0900

    [Silver III] Title: 이전 순열, Time: 100 ms, Memory: 111416 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,13 @@
+N = int(input())
+nums = list(map(int, input().split()))
+
+for i in range(N-1, 0, -1):
+    if nums[i] < nums[i-1]:
+        x, y = i-1, i
+        for j in range(N-1, 0, -1):
+            if nums[j] < nums[x]:
+                nums[j], nums[x] = nums[x], nums[j]
+                nums = nums[:i] + sorted(nums[i:], reverse=True)
+                print(*nums)
+                exit()
+print(-1)
