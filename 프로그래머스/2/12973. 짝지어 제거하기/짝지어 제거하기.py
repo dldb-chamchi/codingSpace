e427b58344f030bfe6f2953a20991ed804259272
임아리 <dladkfl49@gmail.com>
2026-03-18 23:05:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 91.91 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def solution(s):
+    
+    stack = deque()
+    
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    
+    return 0 if stack else 1
